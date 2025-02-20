0d5e31813236036e18bf28bbaf617db0aade2e27
Issa Muiz <muizbabatunde33@gmail.com>
2025-02-19 16:34:49 -0800

    successfully integrate the login routes with the frontend

--- .history/src/app_20250219153653.ts
+++ .history/src/app_20250219153653.ts
@@ -24,13 +24,13 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
-app.use(express.json());
 app.use(helmet());
 app.use(cookieParser());
 app.use(compression());
